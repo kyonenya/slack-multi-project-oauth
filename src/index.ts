a64c54021fde635af715aa0c7049da6f86cfcf81
kyonenya <kyon.musica@gmail.com>
2023-10-21 19:31:14 +0900

    refactor: 素のHTTPで書き直し

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,47 @@
-import { SlackApp, SlackEdgeAppEnv } from 'slack-cloudflare-workers';
+import { Hono } from 'hono';
 
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-  //
-  // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-  // MY_QUEUE: Queue;
-}
+const app = new Hono();
 
-export default {
-  async fetch(
-    request: Request,
-    env: SlackEdgeAppEnv,
-    ctx: ExecutionContext,
-  ): Promise<Response> {
-    const app = new SlackApp({ env });
+app.get('/', async (c) => {
+  return c.json({ message: 'Hello from Hono!' });
+});
 
-    app.event('app_mention', async ({ context }) => {
-      await context.say({
-        text: `<@${context.userId}> さんの teamId は ${context.teamId} です。`,
-      });
+app.post('/slack/events', async (c) => {
+  const body = await c.req.json();
+  const { event, team_id } = body;
+
+  // Verify URL
+  if ('challenge' in body) {
+    return c.json({ challenge: body.challenge });
+  }
+
+  if (event && event.type === 'app_mention') {
+    // TODO: このワークスペース用のOAuthトークンを取得（仮のデータベースから）
+    const oauthToken = c.env?.SLACK_BOT_TOKEN;
+
+    const text = `Your team ID is ${team_id}`;
+
+    // メッセージを送信
+    const response = await fetch('https://slack.com/api/chat.postMessage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${oauthToken}`,
+      },
+      body: JSON.stringify({
+        channel: event.channel,
+        text,
+      }),
     });
 
-    return await app.run(request, ctx);
-  },
-};
+    const data: any = await response.json();
+
+    if (!data.ok) {
+      console.error(`Failed to send message: ${data.error}`);
+    }
+  }
+
+  return c.json({ ok: true });
+});
+
+export default app;
