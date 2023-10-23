467db2f227786b815fa99b380d2201e81833d90f
kyonenya <kyon.musica@gmail.com>
2023-10-23 12:45:27 +0900

    feat: 複数ワークスペースごとにOAuthトークンとプロジェクトIDを出し分け

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono';
 import { drizzle } from 'drizzle-orm/d1';
+import { eq } from 'drizzle-orm';
 import { projects } from './schema';
 import type { SlackOAuthAndOIDCEnv } from 'slack-cloudflare-workers';
 
 const app = new Hono();
+const baseUrl = 'https://participating-trial-handled-parker.trycloudflare.com';
 
 export type Env = SlackOAuthAndOIDCEnv & {
   DB: D1Database;
@@ -16,6 +18,8 @@ app.get('/', async (c) => {
 app.post('/slack/events', async (c) => {
   const body = await c.req.json();
   const { event, team_id } = body;
+  const env = c.env as unknown as Env;
+  const db = drizzle(env.DB);
 
   // Verify URL
   if ('challenge' in body) {
@@ -26,19 +30,19 @@ app.post('/slack/events', async (c) => {
   console.log(team_id);
 
   if (event && event.type === 'app_mention') {
-    // このワークスペース用のOAuthトークンを取得
-    // TODO: データベースに保存して出し分ける
-    const oauthToken = c.env?.SLACK_BOT_TOKEN;
-    // const oauthToken = c.env?.SLACK_BOT_TOKEN_TWO;
+    // DBからリクエスト元ワークスペース用のOAuthトークンとプロジェクトIDを取り出す
+    const { slackBotToken, hfProjectId } = (
+      await db.select().from(projects).where(eq(projects.teamId, team_id))
+    )[0];
 
-    const text = `Your team ID is ${team_id}`;
+    const text = `あなたのプロジェクトIDは ${hfProjectId} です。また、あなたのSlackチームIDは ${team_id} です。`;
 
     // メッセージを送信
     const response = await fetch('https://slack.com/api/chat.postMessage', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${oauthToken}`,
+        Authorization: `Bearer ${slackBotToken}`,
       },
       body: JSON.stringify({
         channel: event.channel,
@@ -70,10 +74,7 @@ app.get('/slack/oauth_redirect', async (c) => {
   params.append('client_id', env.SLACK_CLIENT_ID ?? '');
   params.append('client_secret', env.SLACK_CLIENT_SECRET ?? '');
   params.append('code', code);
-  params.append(
-    'redirect_uri',
-    'https://relax-obligations-province-charles.trycloudflare.com/slack/oauth_redirect',
-  );
+  params.append('redirect_uri', `${baseUrl}/slack/oauth_redirect`);
 
   try {
     const response = await fetch('https://slack.com/api/oauth.v2.access', {
@@ -89,9 +90,10 @@ app.get('/slack/oauth_redirect', async (c) => {
 
     if (data.ok) {
       console.log(`Access token: ${data.access_token}`);
+      // DBにOAuthトークンとプロジェクトIDをチームIDと紐づけて保存
       await db.insert(projects).values({
-        slackBotToken: data.access_token,
         teamId: data.team.id,
+        slackBotToken: data.access_token,
         hfProjectId: state,
       });
     } else {
